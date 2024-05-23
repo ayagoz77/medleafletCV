da991f9ea95a7bec0911f5d9d3dd080fa6e1e85e
Ayagoz <ASaparkhankyzy@beeline.kz>
2024-05-23 22:33:33 +0500

    refactor: small changes

--- utils/text_extractor.py
+++ utils/text_extractor.py
@@ -9,9 +9,12 @@ class TextExtractor:
     def __call__(self, image: np.ndarray) -> List[Tuple[int]]:
 
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite('debug_imgs/gray.png', gray)
         ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+        cv2.imwrite('debug_imgs/thrsh.png', thresh1)
         rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, self.rect_size)
         dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
+        cv2.imwrite('debug_imgs/dilation.png', dilation)
         contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         
         sorted_rects = sorted((cv2.boundingRect(cnt) for cnt in contours), key=lambda rect: (rect[0], rect[1]))
